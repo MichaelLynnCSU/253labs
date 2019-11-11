ecc5ddcd4140cc076302dd42a2989e0ebf318505
Michael Lynn <hailviral101@yahoo.com>
2019-11-11 13:07:26 -0700

    update
    update

--- CoC/CoC.cpp
+++ CoC/CoC.cpp
@@ -9,183 +9,34 @@ using namespace std;
 
 int main()
 {
-	//*****************************************************************************************************
-	// 		As logan's daughter is new to maths , she can't find sum if digits in a string s are in arbitrary order like 3 + 5 + 2.Therefore her father will help her to find the sum by arranging digits in non - decreasing order in string s like 2 + 3 + 5 .
-	//      Your job is to help logan to arrange the digits in non - decreasing order in string s so that her daughter can find the sum.
-	//*****************************************************************************************************
+	
 
-	//{
-
-	//	int X, Y, Z;
-
-	//	cin >> X >> Y >> Z; cin.ignore();
-
-	//	multiset<int> myset;
-
-	//	myset.insert(X);
-
-	//	myset.insert(Y);
-
-	//	myset.insert(Z);
-
-	//	multiset<int>::iterator it = myset.begin();
-
-	//	cout << *it;
-
-	//	++it;
-
-	//	for (; it != myset.end(); ++it)
-	//	{
-
-	//		cout << " + " << *it;
-
-	//	}
-
-	//	cout << endl;
-	//}
-
-	//*****************************************************************************************************
-	// sum of the first x powers of n
-	// Sum of each x powers of n 
-	//*****************************************************************************************************
-
-	//{
-
-	//	int n;
-	//	cin >> n; cin.ignore();
-
-	//	int x;
-	//	cin >> x; cin.ignore();
-
-	//	int sum = 0;
-
-	//	for (int i = 1; i < (x + 1); ++i)
-	//	{
-	//		cout << pow(n, i) << endl;
-
-	//			sum += pow(n, i);
-
-	//	}
-
-	//	cout << sum << endl;
-
-	//}
-
-	//*****************************************************************************************************
-	//Your program must output all the odd numbers from 1 to N where N is given as input.
-	//	If N is an odd number, it must be included in the output.
-	//*****************************************************************************************************
-
-	//{
-	//
-	//	int N;
-	//	cin >> N; cin.ignore();
-
-	//		for (int i = 1; i < N + 1; i++)
-	//		{
-
-	//			if (i % 2 != 0)
-	//			{
-
-	//				cout << i << endl;
-
-	//			}
-
-	//		}
-	//	
-	//}
-
-	//*****************************************************************************************************
-	/*The chart is composed of '#' and '.' (dots) and is given line by line.
-		New properties are given as a list of numbers H, which are new columns' heights.*/
-	//*****************************************************************************************************
-	//CHECK
-	//{
-
-
-	//		string S[100];
-	//		int N;
-	//		cin >> N; cin.ignore();
-	//		for (int i = 0; i < N; i++) {
-	//			getline(cin, S[i]);
-	//		}
-	//		int M;
-	//		cin >> M; cin.ignore();
-	//		for (int i = 0; i < M; i++) {
-	//			int H;
-	//			cin >> H; cin.ignore();
-	//			for (int j = 0; j <= N; j++)
-	//				if (j<H + 1)
-	//					S[N - j] += "#";
-	//				else
-	//					S[N - j] += ".";
-	//		}
-	//		for (int i = 0; i < N; i++)
-	//			cout << S[i] << endl;
-	//	}
-
-	//*****************************************************************************************************
-		//Write a program that prints the sum of all the positive multiples of 3 or 5 or 7 below N.
-		//For example, if N = 15 we get 3, 5, 6, 7, 9, 10, 12, 14 and the sum of these multiples is 66.
-	//*****************************************************************************************************
-
-	//{
-	//
-
-	//	int N;
-	//	cin >> N; cin.ignore();
-	//	int sum = 0;
-
-	//	for (int i = 0; i < N; i++)
-	//	{
-	//		if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0)
-	//		{
-	//			sum += i;
-	//		}
-
-	//	}
-	//	cout << sum;
-	//
-
-	//}
-
-
-	//*****************************************************************************************************
-	/*Construct a square using '+' characters with width and height as 'n'.*/
-	//*****************************************************************************************************
-
-
-	//{
-
-	//int s;
-	//cin >> s; cin.ignore();
-
-	//int s;
-	//cin >> s; cin.ignore();
-	//for (int i = 0; i < s; i++)
-	//{
-	//	for (int j = 0; j < s; j++)
-	//	{
-	//		cout << "+";
-	//	}
-	//	cout << endl;
-	//}
-
-
-	//}
-
-	//*****************************************************************************************************
-	/*Construct a square using '+' characters with width and height as 'n'.*/
-	//*****************************************************************************************************
+	/******************************************************************************************************
+		sum of the first x powers of n
+		Sum of each x powers of n
+	******************************************************************************************************/
 
 	{
-	
-	
-	
-	
-	
-	}
 
+		int n;
+		cin >> n; cin.ignore();
+
+		int x;
+		cin >> x; cin.ignore();
+
+		int sum = 0;
+
+		for (int i = 1; i < (x + 1); ++i)
+		{
+			cout << pow(n, i) << endl;
+
+			sum += pow(n, i);
+
+		}
+
+		cout << sum << endl;
+
+	}
 
 
 
